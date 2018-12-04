51fa18e9c20e5b693bebc47ac831d7959bc80d6e
alexandr.rogansky@gmail.com <alex19wave>
2018-12-04 20:07:34 +0200

    Output list of the items has been implemented

--- client/view.js
+++ client/view.js
@@ -1,19 +1,28 @@
-const api                    = require('../api/config');
-const host                   = api.server.hostname;
-const port                   = api.server.port;
+const host                   = 'http://127.0.0.1';
+const port                   = 3000;
 
 class DataStorage {
     constructor() {
-        this.getData();
+        this.getData('tasks');
+        this.wrapperDiv = document.querySelector('.wrapper');
     };
 
-    getData() {
-        Requests.get(
+    async getData(items) {
+        let resp = await Requests.get(
             {
-                url: `${api.address}/templates`
-            })
-            .then(function (resp) {
-                resp.status == '200' ? showTemplates(resp.json) : alert('Fail!');
+                url: `${host}:${port}/${items}`
             });
+        resp.status === '200' ? new Error('Request is not correct!') : this.showItems(resp.json);
+    }
+
+    showItems(itemsJSON) {
+        itemsJSON.forEach((item) => {
+            let {date, description} = item;
+            let itemDiv = document.createElement('div');
+            itemDiv.innerText = `${description}, ${new Date(date).toDateString()}`;
+            this.wrapperDiv.appendChild(itemDiv);
+        })
     }
 }
+
+new DataStorage();
